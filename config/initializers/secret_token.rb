75054cc862dfd46bf26ad8de31dd0ad66e7291e8
Mike Moore <mike@blowmage.com>
2012-07-05 20:40:53 -0600

    Initialize empty Rails project
    Hello world\!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pairwithme::Application.config.secret_token = 'a91ccabb3399a8c12b880782d7b560d2fc14e3189f9c6895ec95f564f1bd501cfc66c8c63e31f65e5b04f0ab1d76538ea474dc3336434722a9e4c26329cdffb4'
